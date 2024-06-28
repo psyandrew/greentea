83e99eb68efb4fa7e947299b364ed97570cd1479
psyandrew <casero.andrew@gmail.com>
2024-06-28 13:36:02 +0800

    June 28 Commit

--- src/Feature2.js
+++ src/Feature2.js
@@ -1,30 +1,9 @@
-export default function Feature2() {
+xport default function Feature2() {
 
 
   return (
     <div>
-      <div className="featurefloor">
-          <div className='panelcolumn col'>
-            <div className='panelcontainer'>
-              <div className="panel1">
-                <div className="overlay">
-                  <h2 className="panel1text font-playfair">GRAB A FRESH BAG</h2>
-                </div>                
-              </div>
-              <div className="panel2">
-                <div className="overlay">
-                  <h1 className="panel2text font-playfair goldfont">OPENING SOON:<br/></h1><h1 className='panel2text font-playfair'> OUR TEA GARDEN CAFE</h1>
-                </div> 
-             </div>
-              <div className="panel3">
-                <div className="overlay">
-                  <h2 className='panel3text font-playfair'>BOOK A TOUR OF OUR TEA FIELDS</h2>
-                </div> 
-             </div>
-            </div>
-          </div>
 
-      </div>
     </div>
   );
 }
